173a5b66292d10257349b033e13b7051c0fd36c1
nightly haze <155249497+nightlyhaze@users.noreply.github.com>
2026-01-29 13:51:35 +0800

    Update runWWV.sh

--- runWWV.sh
+++ runWWV.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd ~/LinuxWWV
+cd ~/LinuxWWV #edit this if you did not clone the project in your home directory
 sudo sh updateRTCtoNTP.sh
 
+g++ -std=c+=17 linuxwwv.cpp -o LinuxWWV
 ./LinuxWWV
 #sudo shutdown --no-wall now
-
